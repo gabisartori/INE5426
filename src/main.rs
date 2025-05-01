dd4d0529b06d5ddc2fd876c7359da0ecc9e3bd6e
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-05-01 02:04:40 -0300

    Update main.rs to handle inputs via text files instead of having it hardcoded

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::{HashSet, HashMap};
-use std::hash::Hash;
+// use std::hash::Hash;
 
 type State = u32;
 type Symbol = char;
@@ -15,16 +15,16 @@ struct FSDA {
   initial_state: State,
   final_states: HashSet<State>,
   transitions: HashMap<(State, Symbol), (State, Option<Command>)>,
-  states: HashSet<State>,
-  alphabet: HashSet<Symbol>,
+  _states: HashSet<State>,
+  _alphabet: HashSet<Symbol>,
 }
 
 impl FSDA {
   fn new(initial_state: State, final_states: HashSet<State>, transitions: HashMap<(State, Symbol), (State, Option<Command>)>) -> FSDA {
-    let states = transitions.keys().map(|(state, _)| *state).collect::<HashSet<_>>();
-    let alphabet = transitions.keys().map(|(_, symbol)| *symbol).collect::<HashSet<_>>();
+    let _states = transitions.keys().map(|(state, _)| *state).collect::<HashSet<_>>();
+    let _alphabet = transitions.keys().map(|(_, symbol)| *symbol).collect::<HashSet<_>>();
 
-    FSDA { initial_state, final_states, transitions, states, alphabet }
+    FSDA { initial_state, final_states, transitions, _states, _alphabet }
   }
 
   fn accepts(&self, input: Vec<Symbol>) -> bool {
@@ -40,20 +40,30 @@ impl FSDA {
   }
 }
 
-fn main() {
-  let initial_state = S0;
-  let final_states = HashSet::from([S0]);
-  let transitions = HashMap::from([
-    ((S0, A0), (S0, None)),
-    ((S0, A1), (S1, None)),
-    ((S1, A0), (S2, None)),
-    ((S1, A1), (S0, None)),
-    ((S2, A0), (S1, None)),
-    ((S2, A1), (S2, None)),
-  ]);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  // Read args
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() < 2 { return Err("Usage: <input_file>".into()); }
+  let input_file = &args[1];
+  let input = std::fs::read_to_string(input_file)?;
+  let lines = input.lines();
 
-  let fsa = FSDA::new(initial_state, final_states, transitions);
-  let input = "0111".chars().collect::<Vec<Symbol>>();
-  let result = fsa.accepts(input);
-  println!("Input accepted: {}", result);
+  for line in lines {
+    let initial_state = S0;
+    let final_states = HashSet::from([S0]);
+    let transitions = HashMap::from([
+      ((S0, A0), (S0, None)),
+      ((S0, A1), (S1, None)),
+      ((S1, A0), (S2, None)),
+      ((S1, A1), (S0, None)),
+      ((S2, A0), (S1, None)),
+      ((S2, A1), (S2, None)),
+    ]);
+
+    let fsa = FSDA::new(initial_state, final_states, transitions);
+    let input = line.chars().collect::<Vec<Symbol>>();
+    let result = fsa.accepts(input);
+    println!("Input {} accepted: {}", line, result);
+  }
+  Ok(())
 }
